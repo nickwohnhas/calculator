013b1d483632661f8b4a62d62729c3a5e6294000
Nick Wohnhas <npw7887@gmail.com>
2019-10-03 10:35:11 -0400

    Add computation logic and state

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,47 +1,81 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Calculator = () => (
+const Calculator = () => {
+  const handleClick = (e) => {
+    if (restartComputation) {
+      setLastValue(e.target.textContent)
+      setComputation(e.target.textContent)
+      setRestartComputation(false)
+    } else {
+      if (isNaN(parseInt(e.target.textContent)) || isNaN(parseInt(lastValue))) {
+        setComputation(computation + " " + e.target.textContent)
+      } else {
+        setComputation(computation + e.target.textContent)
+      }
+      setLastValue(e.target.textContent)
+    }
+  }
 
-  <div>
-    <header className="header">
-      <h1>Calculator App</h1>
-    </header>
-    <div id="calculator">
-    <section id="display">
-        0
+  const handleEquals = () => {
+    let sanitizedString = computation.replace(/x/g, "*")
+    let evaluation = eval(sanitizedString)
+    setComputation(evaluation)
+    setRestartComputation(true)
+  }
+
+  const handleClear = () => {
+    setComputation("")
+    setRestartComputation(false)
+    setLastValue("")
+  }
+
+  const [computation, setComputation] = useState("")
+  const [restartComputation, setRestartComputation] = useState(false)
+  const [lastValue, setLastValue] = useState("")
+
+  return (
+    <div>
+      <header className="header">
+        <h1>Calculator App</h1>
+      </header>
+      <div id="calculator">
+        <section id="display">
+          {computation}
     </section>
-      <div className="clr"></div>
-      <section id="numbers">
-        <div className="row">
-          <div className="number">7</div>
-          <div className="number">4</div>
-          <div className="number">1</div>
-        </div>
+        <div className="clr"></div>
+        <section id="numbers">
+          <div className="row">
+            <div onClick={handleClick} className="number">7</div>
+            <div onClick={handleClick}className="number">4</div>
+            <div onClick={handleClick}className="number">1</div>
+          </div>
 
-        <div className="row">
-          <div className="number">8</div>
-          <div className="number">5</div>
-          <div className="number">2</div>
-        </div>
+          <div className="row">
+            <div onClick={handleClick}className="number">8</div>
+            <div onClick={handleClick}className="number">5</div>
+            <div onClick={handleClick}className="number">2</div>
+          </div>
 
-        <div className="row">
-          <div className="number">9</div>
-          <div className="number">6</div>
-          <div className="number">3</div>
-        </div>
-      </section>
-      <section id="operations">
-        <div className="number">/</div>
-        <div className="number">x</div>
-        <div className="number">-</div>
-      </section>
-      <section id="bottom">
-        <div className="zero number">0</div>
-        <div className="lowest number">=</div>
-        <div className="lowest number">+</div>
-      </section>
+          <div className="row">
+            <div onClick={handleClick}className="number">9</div>
+            <div onClick={handleClick}className="number">6</div>
+            <div onClick={handleClick}className="number">3</div>
+          </div>
+        </section>
+        <section id="operations">
+          <div onClick={handleClick}className="number">/</div>
+          <div onClick={handleClick}className="number">x</div>
+          <div onClick={handleClick}className="number">-</div>
+        </section>
+        <section id="bottom">
+          <div onClick={handleClick}className="zero number">0</div>
+          <div onClick={handleClear} className="lowest number">c</div>
+          <div onClick={handleEquals} className="lowest number">=</div>
+          <div onClick={handleClick}className="lowest number">+</div>
+        </section>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Calculator
