b7d99d99648efc4861244e5ada5046dad3946302
nickwohnhas <npw7887@gmail.com>
2020-01-06 18:54:41 -0500

    Round floats to 2 decimals
    This is a quick fix to prevent floats from leaving the evaluation container

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,47 +1,56 @@
-import React, {useState} from 'react'
-import Interface from './Interface'
+import React, { useState } from 'react';
+import Interface from './Interface';
 
 const Calculator = () => {
-  const handleClick = (e) => {
+  const handleClick = e => {
     if (restartComputation) {
-      setLastValue(e.target.textContent)
-      setComputation(e.target.textContent)
-      setRestartComputation(false)
+      setLastValue(e.target.textContent);
+      setComputation(e.target.textContent);
+      setRestartComputation(false);
+    } else if (
+      isNaN(parseInt(e.target.textContent)) ||
+      isNaN(parseInt(lastValue))
+    ) {
+      setComputation(computation + ' ' + e.target.textContent);
     } else {
-      if (isNaN(parseInt(e.target.textContent)) || isNaN(parseInt(lastValue))) {
-        setComputation(computation + " " + e.target.textContent)
-      } else {
-        setComputation(computation + e.target.textContent)
-      }
-      setLastValue(e.target.textContent)
+      setComputation(computation + e.target.textContent);
     }
-  }
+    setLastValue(e.target.textContent);
+  };
 
   const handleEquals = () => {
-    let sanitizedString = computation.replace(/x/g, "*")
-    let evaluation = eval(sanitizedString)
-    setComputation(evaluation)
-    setRestartComputation(true)
-  }
+    let sanitizedString = computation.replace(/x/g, '*');
+    let evaluation = eval(sanitizedString);
+    if (!Number.isInteger(evaluation)) {
+      evaluation = evaluation.toFixed(2);
+    }
+    setComputation(evaluation);
+    setRestartComputation(true);
+  };
 
   const handleClear = () => {
-    setComputation("")
-    setRestartComputation(false)
-    setLastValue("")
-  }
+    setComputation('');
+    setRestartComputation(false);
+    setLastValue('');
+  };
 
-  const [computation, setComputation] = useState("")
-  const [restartComputation, setRestartComputation] = useState(false)
-  const [lastValue, setLastValue] = useState("")
+  const [computation, setComputation] = useState('');
+  const [restartComputation, setRestartComputation] = useState(false);
+  const [lastValue, setLastValue] = useState('');
 
   return (
     <div>
-      <header className="header">
+      <header className='header'>
         <h1>Calculator App</h1>
       </header>
-      <Interface computation={computation} handleClear={handleClear} handleClick={handleClick} handleEquals={handleEquals} />
+      <Interface
+        computation={computation}
+        handleClear={handleClear}
+        handleClick={handleClick}
+        handleEquals={handleEquals}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
